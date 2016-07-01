992746dc64eccd926504b3f3d695ed9d7e34aa3d
jros <jros@zertifica.com>
2016-07-01 13:16:06 +0200

    corrections to packaging again

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='elavonvtpv',
   packages=['elavonvtpv'],
-  version='1.1',
+  version='1.2',
   description='Python 3 package to comunicate with the Elavon Virtual TPV Remote API',
   author='Javier Ros Honduvilla',
   author_email='javier.ros.honduvilla@gmail.com',
   url='https://github.com/jros99/elavontpv',
-  download_url='https://github.com/jros99/elavontpv/tarball/0.2',
+  download_url='https://github.com/jros99/elavontpv/tarball/0.3',
   keywords=['elavon', 'tpv', 'virtual tpv'],
   classifiers=[],
 )
