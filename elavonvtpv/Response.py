27a4e107974792b7751ac48c76e53b2e51d071b3
jros <jros@zertifica.com>
2016-07-06 10:12:31 +0200

    fixed attribute handling and added a way to know the tags inside the wrapped xml in the Response

--- elavonvtpv/Response.py
+++ elavonvtpv/Response.py
@@ -4,14 +4,17 @@ import hashlib
 
 
 class Response:
-    def __init__(self, xml):
+    def __init__(self, xml=None):
         """
         Defines a Response object
         :param xml: a string containing xml data
         """
-        self._xml = Etree.fromstring(xml)
+        if xml:
+            self._xml = Etree.fromstring(xml)
+        else:
+            self._xml = None
 
-    def find(self, item):
+    def __getattr__(self, item):
         """
         Search unidentified attributes inside the ElementTree structure inside the _xml attribute.
         :param item: the name of the requested attribute
@@ -19,11 +22,26 @@ class Response:
         """
         return self._xml.find(item).text
 
+    def list_xml_tags(self):
+        """
+        Returns a list of all xml tags inside the object.
+        :return: list of strings representing the tags of the xml elements contained by the object.
+        """
+        return [elem.tag for elem in self._xml.iter()]
+
     def to_xml_string(self):
+        """
+        Turns the xml tree contained by this object into a string.
+        :return: a string with the xml contained by this object.
+        """
         binary = Etree.tostring(self._xml, encoding='utf8', method='xml')
         return binary.decode('utf-8')
 
     def to_pretty_xml(self):
+        """
+        Turns the xml tree contained by this object into a pretty string for easy reading.
+        :return: a string with the xml contained by this object.
+        """
         return minidom.parseString(self.to_xml_string()).toprettyxml()
 
     def __hash(self):
